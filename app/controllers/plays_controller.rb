9418ed4938e45660aacf2dcaabd60c12e72f6569
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-02-03 00:19:58 -0500

    Add user authentication

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,5 +1,6 @@
 class PlaysController < ApplicationController
   before_action :find_play, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
   # before_action :set_category, only: [:show, :edit, :update, :destroy]
   def index
     if params[:category].blank?
@@ -44,6 +45,11 @@ class PlaysController < ApplicationController
   end
 
   def show
+    if @play.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @play.reviews.average(:rating).round(2)
+    end
   end
 
   def destroy
